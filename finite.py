8762475e91dabed51cba7fa23d79dd819474710d
Iñaki Liendo <sebastian@liendo.net>
2025-08-10 16:42:52 -0600

    review + binary search

--- finite.py
+++ finite.py
@@ -64,6 +64,68 @@ def non_negative_int_sol(q, rhs, gcd, bez):
     return None
 
 
+def non_negative_int_sol_bin_search(q, rhs, gcd, bez):
+    n = len(q)
+
+    # particular solutions
+    x_b, w_b = bez[0]
+
+    # feasible parameters
+    lb = math.ceil(-rhs * x_b / gcd[1])
+    ub = math.floor(rhs * w_b / q[0])
+
+    # pass-through stack
+    stack = [[0, lb, ub, rhs, []]]
+
+    while len(stack) > 0:
+        i, lb, ub, rhs, path = stack.pop()
+
+        # search space is empty. backtracking
+        if ub < lb:
+            continue
+
+        # binary search on same level
+        t = (ub + lb) // 2
+        if t + 1 <= ub:
+            stack.append([i, t + 1, ub, rhs, path])
+        if t - 1 >= lb:
+            stack.append([i, lb, t - 1, rhs, path])
+
+        # prev equation solutions
+        x_b, w_b = bez[i]
+        x_p = rhs * x_b + t * gcd[i + 1]
+        rhs = rhs * w_b - t * q[i]
+
+        # last equation
+        if i == n - 3:
+            x_b, y_b = bez[-1]
+
+            # check if there exists a feasible parameter
+            lb = math.ceil(-rhs * x_b / q[-1])
+            ub = math.floor(rhs * y_b / q[-2])
+
+            if ub < lb:
+                continue
+
+            # last two nonnegative solutions
+            x = rhs * x_b + lb * q[-1]
+            y = rhs * y_b - lb * q[-2]
+
+            # unpack previous solutions
+            return path + [x_p, x, y]
+
+        # feasible parameters
+        x_b, w_b = bez[i + 1]
+        lb = math.ceil(-rhs * x_b / gcd[i + 2])
+        ub = math.floor(rhs * w_b / q[i + 1])
+
+        # push next level
+        if lb <= ub:
+            stack.append([i + 1, lb, ub, rhs, path + [x_p]])
+
+    return None
+
+
 @repeat_with_timeout()
 def dioph(q, eta):
     _q = q.copy()
@@ -82,7 +144,7 @@ def dioph(q, eta):
 
     # find first nonnegative solution to dot(q, x) = eta
     while eta >= 0:
-        x = non_negative_int_sol(_q, eta, gcd, bez)
+        x = non_negative_int_sol_bin_search(_q, eta, gcd, bez)
         if x is not None:
             return np.asarray(x)
         eta -= 1
@@ -93,13 +155,10 @@ def dioph(q, eta):
 if __name__ == "__main__":
     from src.common import (
         stats_dioph_as_rhs_increases as dioph_rhs,
-        stats_dp_as_rhs_increases as dp_rhs,
         stats_dioph_as_dim_increases as dioph_dim,
-        stats_dp_as_dim_increases as dp_dim,
-        stats_bb_as_dim_increases as bb_dim,
     )
     from src.common import run_parallel
-    from src.constants import RANDOM_SEED, bb_raw_options, bb_full_options
+    from src.constants import RANDOM_SEED
 
     rng = np.random.default_rng(seed=RANDOM_SEED)
     p = np.sort(rng.integers(10, 10_000, size=1_000))
@@ -139,34 +198,14 @@ if __name__ == "__main__":
     )
     jobs = [
         {
-            "name": "dioph-v2",
-            "log_path": create_rhs_path("dioph-v2"),
+            "name": "dioph-bin",
+            "log_path": create_rhs_path("dioph-bin"),
             "job": lambda *_: dioph_rhs(dioph, q[::-1].copy(), m, rhs.copy()),
         },
         {
-            "name": "dp",
-            "log_path": create_rhs_path("dp"),
-            "job": lambda *_: dp_rhs(p.copy(), rhs.copy()),
-        },
-        {
-            "name": "dioph-v2",
-            "log_path": create_dim_path("dioph-v2"),
+            "name": "dioph-bin",
+            "log_path": create_dim_path("dioph-bin"),
             "job": lambda *_: dioph_dim(dioph, dims.copy()),
-        },
-        {
-            "name": "dp",
-            "log_path": create_dim_path("dp"),
-            "job": lambda *_: dp_dim(dims.copy()),
-        },
-        {
-            "name": "bb_raw",
-            "log_path": create_dim_path("bb_raw"),
-            "job": lambda *_: bb_dim(dims.copy(), **bb_raw_options),
-        },
-        {
-            "name": "bb_full",
-            "log_path": create_dim_path("bb_full"),
-            "job": lambda *_: bb_dim(dims.copy(), **bb_full_options),
         },
     ]
     run_parallel(jobs)
